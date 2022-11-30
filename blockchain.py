6ef119d387f78d98533d0207e62b956611d9601a
Miaurycy <eryktomkowicz123@gmail.com>
2022-11-30 01:05:53 +0100

    Create a basic User Input Interface

--- blockchain.py
+++ blockchain.py
@@ -17,19 +17,42 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     """Returns the input of the user (a new transaction amount) as a float."""
-    return float(input("Your transaction amount please: "))
+    user_input = float(input("Your transaction amount please: "))
+    return user_input
 
 
-tx_amount = get_user_input()
+def get_user_choice():
+    """Gets an input of the user to select an option from the user interface."""
+    user_input = input("Your choice: ")
+    return user_input
+
+
+def print_blockchain_elements():
+    """Output the blockchain list to the console."""
+    for block in blockchain:
+        print("Outputting Block")
+        print(block)
+
+
+tx_amount = get_transaction_value()
 add_value(tx_amount)
 
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(),
-          transaction_amount=tx_amount)
+while True:
+    print("Please choose")
+    print("1: Add a new transaction value")
+    print("2: Output the blockchain blocks")
+    print("q: Quit")
+    user_choice = get_user_choice()
+    if user_choice == "1":
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    elif user_choice == "2":
+        print_blockchain_elements()
+    elif user_choice == "q":
+        pass
+    else:
+        print("Input was invalid, please pick a value from the list!")
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
-
-print(blockchain)
+print("Done!")
