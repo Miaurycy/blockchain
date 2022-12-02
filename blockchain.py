e3a438b9fe5d09ecd01a4316c0321bb5350ae4e4
Miaurycy <eryktomkowicz123@gmail.com>
2022-12-02 16:31:13 +0100

    Add a function verifies the blockchain

--- blockchain.py
+++ blockchain.py
@@ -38,12 +38,27 @@ def print_blockchain_elements():
     for block in blockchain:
         print("Outputting Block")
         print(block)
+    else:
+        print("-" * 20)
+
+def verify_chain():
+    is_valid = True
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        elif blockchain[block_index][0] != blockchain[block_index - 1]:
+            is_valid = False
+            break
+    return is_valid
 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print("Please choose")
     print("1: Add a new transaction value")
     print("2: Output the blockchain blocks")
+    print("h: Manipulate the chain")
     print("q: Quit")
     user_choice = get_user_choice()
     if user_choice == "1":
@@ -51,9 +66,17 @@ while True:
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == "2":
         print_blockchain_elements()
+    elif user_choice == "h":
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == "q":
-        break
+        waiting_for_input = False
     else:
         print("Input was invalid, please pick a value from the list!")
-    print("Choice registered!")
+    if not verify_chain():
+        print("Invalid blockchain!")
+        print_blockchain_elements()
+        break
+else:
+    print("User left!")
 print("Done!")
